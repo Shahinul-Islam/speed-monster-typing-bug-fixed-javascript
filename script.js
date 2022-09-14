e1b87ef7f5127d2d1076a2dd3844cd6b5a690a7e
shahinul <shahinul37@outlook.com>
2022-09-14 22:33:58 +0600

    seconds count in decimal fixed

--- script.js
+++ script.js
@@ -137,7 +137,9 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  console.log(currentTime);
+  const timeSpent = ((currentTime - startTime) / 1000).toFixed(0);
+  console.log(timeSpent);
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
